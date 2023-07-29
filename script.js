e1752dcf34e862c3196858c91caa330aa0e5420e
YidnekachewTefera <yomaytec@gmail.com>
2023-07-29 12:57:44 -0700

    added delete functionalities

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+const bodyy = document.querySelector("body");
 const addBook = document.querySelector(".addButton");
 const container = document.querySelector(".main-wrapper");
 const booksPlate = document.querySelector(".books-form");
 const books = document.querySelector(".books");
-const bookList = [];
+const bookList = JSON.parse(localStorage.getItem("myLibrary")) || [];
 addBook.addEventListener("click", () => {
   container.classList = "displayForm";
 });
@@ -16,10 +17,57 @@ function addBooks(e) {
     title,
     author,
     page,
-    done: false,
+    read_status: false,
   };
   bookList.push(book);
-  console.log(bookList);
+  populateList(bookList, books);
+  localStorage.setItem("myLibrary", JSON.stringify(bookList));
   this.reset();
 }
 booksPlate.addEventListener("submit", addBooks);
+function populateList(book = [], booksContainer) {
+  booksContainer.innerHTML = book.map((boo, i) => {
+    return `
+    <div >
+      <ul>
+          <li id="title${i}">Title:  ${boo.title}</li>
+          <li id="author${i}">Author: ${boo.author}</li>
+          <li id="pageOf${i}">Pages: ${boo.page}</li>
+          <li id="readStatusOf${i}">
+              <div class="readStatus">Read</div>
+          </li>
+          <li class="delete-edit-button-container"><button class="delete" id="delete" data-index=${i}> Delete</button><button class="edit">Edit</button></li>
+      </ul>
+    </div>
+    `;
+  });
+}
+populateList(bookList, books);
+const deletee = document.querySelector(".delete");
+/*function deletionAvoided() {}
+function deleteConfirmation(e) {
+  const card = this.parentElement.parentElement.parentElement;
+  function deleteBook(containerr) {
+    containerr.remove();
+  }
+  const dialogBox = document.createElement("div");
+  dialogBox.backgroundColor=white;
+  dialogBox.textAlign; 
+  console.log(dialogBox);
+  /*`
+  <div style="background-color:white; text-align:center">
+    <h1>Are you sure?</h1>
+    <button class="yes" style="background-color:red">Yes</button><button class="no" style="background-color:green">No</button>
+  </div>
+`*/
+/*bodyy.append(dialogBox);
+  const yes = document.querySelector(".yes");
+  const no = document.querySelector(".no");
+  yes.addEventListener("click", deleteBook(card));
+  no.addEventListener("click", deletionAvoided);
+}
+*/
+function deleteBook(e) {
+  this.parentElement.parentElement.parentElement.remove();
+}
+deletee.addEventListener("click", deleteBook);
